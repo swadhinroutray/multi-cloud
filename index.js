f5d2725effba81dab6073ec34237cdbf91df5ac8
swadhinroutray <swadhin.routray@gmail.com>
2020-09-07 11:42:42 +0530

    changed post to get and incorporated city value in parameters

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/provider/:id", async (req, res) => {
+app.get("/provider/:id/:city", async (req, res) => {
   console.log("hello this is basic function");
   const id = req.params.id;
   console.log(id);
@@ -25,7 +25,7 @@ app.post("/provider/:id", async (req, res) => {
 });
 
 async function AWS(req, res) {
-  const city = req.body.city;
+  const city = req.params.city;
   let response = await axios.post(process.env.AWS_URI + "/weather-app", {
     city: city,
   });
@@ -35,7 +35,7 @@ async function AWS(req, res) {
 }
 
 async function IBM(req, res) {
-  const city = req.body.city;
+  const city = req.params.city;
   let response = await axios.post(
     process.env.IBM_URI + "/weather",
     {
@@ -52,7 +52,7 @@ async function IBM(req, res) {
   res.send({ message: obj.text, provider: obj.cloud });
 }
 async function callLocal(req, res) {
-  let city = req.body.city;
+  let city = req.params.city;
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`;
   let response = await axios.post(url);
   // console.log(response)
